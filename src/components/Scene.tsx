fa0a9315202d38e9f1dba181b55b822878ce7d6b
fusioneventsak <arthurk@fusion-events.ca>
2025-06-17 21:30:30 -0400

    Updated Scene.tsx

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -12,12 +12,15 @@ export default function Scene({ scrollY, currentPage }: SceneProps) {
   const { camera, gl, scene } = useThree();
   
   useEffect(() => {
-    // Set up camera
-    camera.position.set(0, 0, 5);
+    // Set up camera - much closer for sharp focus
+    camera.position.set(0, 0, 3);
     camera.fov = 75;
     camera.near = 0.1;
     camera.far = 1000;
     camera.updateProjectionMatrix();
+    
+    // Look at the center
+    camera.lookAt(0, 0, 0);
     
     // Set up renderer
     gl.setClearColor('#000000', 0); // Transparent background
@@ -41,19 +44,19 @@ export default function Scene({ scrollY, currentPage }: SceneProps) {
       <directionalLight position={[5, 5, 5]} intensity={1.0} />
       <pointLight position={[0, 0, 5]} intensity={0.5} />
       
-      {/* Test objects to confirm 3D is working */}
+      {/* Test objects to confirm 3D is working - positioned closer */}
       <mesh position={[0, 0, 0]}>
-        <boxGeometry args={[0.5, 0.5, 0.5]} />
+        <boxGeometry args={[0.3, 0.3, 0.3]} />
         <meshBasicMaterial color="#ff0000" />
       </mesh>
       
-      <mesh position={[1, 0, 0]}>
-        <sphereGeometry args={[0.2, 16, 16]} />
+      <mesh position={[0.8, 0, 0]}>
+        <sphereGeometry args={[0.15, 16, 16]} />
         <meshBasicMaterial color="#00ff00" />
       </mesh>
       
-      <mesh position={[-1, 0, 0]}>
-        <cylinderGeometry args={[0.2, 0.2, 0.5, 8]} />
+      <mesh position={[-0.8, 0, 0]}>
+        <cylinderGeometry args={[0.15, 0.15, 0.3, 8]} />
         <meshBasicMaterial color="#0000ff" />
       </mesh>
       
